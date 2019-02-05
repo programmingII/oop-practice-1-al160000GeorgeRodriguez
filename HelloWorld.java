205205fb970468721a6f4e3df37da00c9090d06f
geoxneo <geoxneo@gmail.com>
2019-02-05 13:39:24 -0700

    Primer commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//Autor: George Rodríguez
+//Fecha y hora: 5/2/2019 12:30
+//Hace: Imprime dos mensajes
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+        System.out.println("Alexandra Abramov");
+    }
+}
---
+++ SumTwoNumbers.java
@@ -0,0 +1,10 @@
+//Autor: George Rodríguez
+//Fecha y hora: 5/2/2019 12:30
+//Hace: Imprime dos mensajes
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Suma de dos nnumeros ");
+        System.out.println("Ingrese el primer numero: ");
+        System.out.println("El resultado es: ");
+    }
+}
