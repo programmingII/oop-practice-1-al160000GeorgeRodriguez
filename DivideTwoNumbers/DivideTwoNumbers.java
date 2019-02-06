9c5be26bde436e1c96fd395a157bea5dc9601d34
geoxneo <geoxneo@gmail.com>
2019-02-05 17:53:43 -0700

    tresEjercicios

---
+++ DivideTwoNumbers/DivideTwoNumbers.java
@@ -0,0 +1,6 @@
+class DivideTwoNumbers{
+    public static void main(String[] args){
+        System.out.println(50/3);
+        
+    }
+}
---
+++ SumTwoNumbers/SumTwoNumbers.java
@@ -0,0 +1,11 @@
+//Autor: George Rodríguez
+//Fecha y hora: 5/2/2019 12:40
+//Hace: suma dos variables
+
+class SumTwoNumbers {
+    public static void main(String[] args) {
+      
+        System.out.println("Suma de dos numeros ");
+        System.out.println("El resultado de sumar 2+2 es: "+(2+2));
+    }
+}
