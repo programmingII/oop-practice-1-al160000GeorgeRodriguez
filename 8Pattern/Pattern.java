8a45e4299da2a182b3c865efb246569686e5d152
geoxneo <geoxneo@gmail.com>
2019-02-05 19:16:55 -0700

    Mas ejercicios

---
+++ 8Pattern/Pattern.java
@@ -0,0 +1,11 @@
+//Autor: George Rodríguez
+//Fecha y hora: 5/2/2019 13:10
+//Hace: Write a Java program to display the following pattern.
+class Pattern{
+    public static void main(String[] Args){
+        System.out.println("    J     a     v     v   a");
+        System.out.println("    J   a  a     v   v   a  a");
+        System.out.println("J   J  a aa a     v v   a aa a");
+        System.out.println("  J   a      a     v   a      a");
+    }
+}
