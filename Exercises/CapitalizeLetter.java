520fdf197c9d317652c98380807ae3af61d2c516
geoxneo <geoxneo@gmail.com>
2019-02-24 00:00:59 -0700

    Nuevo envío

--- Exercises/CapitalizeLetter.java
+++ Exercises/CapitalizeLetter.java
@@ -1,5 +1,18 @@
 // Autor: George Rodríguez
 // Fecha y hora: 22/2/2019 19:29
-// Hace: Numeros entre rangos
+// Hace: Coloca la primera letra en mayuscula
 import java.util.Scanner; // Libreria que permite el acceso de datos desde consola
-public class IntFactors {
+public class CapitalizeLetter {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);  // Se instancia los objetos
+        System.out.print("Ingresa una frase: "); //Se le pide al usuario su frase
+        String MiLinea = entrada.nextLine();   ///Se ingresa la linea
+        String lineaPrincipal = ""; //Al procesar la linea ingresada se guardará aqui
+        Scanner lineScan = new Scanner(MiLinea);  //Se instancia con la linea ingresada
+        while (lineScan.hasNext()) {   // hasta el final de la linea
+            String frase = lineScan.next();  // Se obtine la siguiente frase
+            lineaPrincipal += Character.toUpperCase(frase.charAt(0)) + frase.substring(1) + " ";  //Se agregan las palabras al texto final
+        }
+        System.out.println(lineaPrincipal.trim()); //Se imprime la linea
+    }
+}
